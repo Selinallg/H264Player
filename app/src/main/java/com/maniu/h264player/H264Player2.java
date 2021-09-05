eae34efca5ac86b95e153fe9abc28bdbb638abfa
dell-liu.lg@163.com <liu.lg@163.com>
2021-09-05 10:41:13 +0800

    llg 提交 验证了 h264 同步异步解码

---
+++ app/src/main/java/com/maniu/h264player/H264Player2.java
@@ -0,0 +1,244 @@
+package com.maniu.h264player;
+
+import android.content.Context;
+import android.media.MediaCodec;
+import android.media.MediaFormat;
+import android.util.Log;
+import android.view.Surface;
+
+import androidx.annotation.NonNull;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
+/**
+ * 异步解码
+ */
+public class H264Player2 implements Runnable {
+    private static final String  TAG = "H264Player";
+    private              Context context;
+
+    private String     path;
+    //mediaCodec   手机硬件不一样    dsp  芯片  不一样
+    //    解码H264  解压     android 硬编  兼容   dsp  1ms   7000k码率   700k码率    4k   8k
+//    码率  直接奔溃 联发科  ----》     音频
+    private MediaCodec mediaCodec;
+    //画面
+    private Surface    surface;
+    private byte[]     bytes;
+
+    public H264Player2(Context context, String path, Surface surface) {
+
+        this.surface = surface;
+        this.path = path;
+        this.context = context;
+
+        try {
+//            h265  --ISO hevc  兼容 硬编   不兼容   电视    -----》8k  4K
+            try {
+                mediaCodec = MediaCodec.createDecoderByType("video/avc");
+            } catch (Exception e) {
+//                不支持硬编
+            }
+
+//            MediaFormat mediaformat = MediaFormat.createVideoFormat("video/avc", 368, 384);
+            MediaFormat mediaformat = MediaFormat.createVideoFormat("video/avc", 2880, 1600);
+//            MediaFormat mediaformat = MediaFormat.createVideoFormat("video/avc", 4320, 2160);
+//            MediaFormat mediaformat = MediaFormat.createVideoFormat("video/avc", 2160, 4320);
+
+//            MediaFormat mediaformat = MediaFormat.createVideoFormat("video/avc", 368, 384);
+//            mediaformat.setInteger(MediaFormat.KEY_FRAME_RATE, 15);
+
+            if (useAsyncDecode) {
+                mediaCodec.setCallback(callback);
+            }
+            mediaCodec.configure(mediaformat, surface, null, 0);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //MediaExtractor  视频      画面H264
+    public void play() {
+
+
+
+        //java线程本质是什么线程         linux线程
+
+
+        new Thread(this).start();
+    }
+
+    @Override
+    public void run() {
+        try {
+
+            bytes = null;
+            try {
+//            偷懒   文件  加载内存     文件 1G  1G
+                bytes = getBytes(path);
+                totalSize = bytes.length;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            mediaCodec.start();
+
+            if (!useAsyncDecode) {
+                decodeH264();
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "run: " + e);
+        }
+    }
+
+    long lastTimeMillis;
+
+    private void decodeH264() {
+
+//内部的队列     不是每一个都可以用
+        ByteBuffer[] inputBuffers = mediaCodec.getInputBuffers();
+
+//
+        int startIndex = 0;
+//总字节数
+        int totalSize = bytes.length;
+        while (true) {
+            if (totalSize == 0 || startIndex >= totalSize) {
+                break;
+            }
+//            寻找索引
+            int nextFrameStart = findByFrame(bytes, startIndex + 2, totalSize);
+
+            MediaCodec.BufferInfo info = new MediaCodec.BufferInfo();
+//            查询哪一个bytebuffer能够用
+            int inIndex = mediaCodec.dequeueInputBuffer(10000);
+            if (inIndex >= 0) {
+//                Log.d(TAG, "decodeH264: -------------------inIndex---------"+inIndex);
+//            找到了  david
+                ByteBuffer byteBuffer = inputBuffers[inIndex];
+                byteBuffer.clear();
+                byteBuffer.put(bytes, startIndex, nextFrameStart - startIndex);
+//
+                mediaCodec.queueInputBuffer(inIndex, 0, nextFrameStart - startIndex, 0, 0);
+                startIndex = nextFrameStart;
+            } else {
+                continue;
+            }
+
+//            得到数据
+            int outIndex = mediaCodec.dequeueOutputBuffer(info, 10000);
+
+//            Log.d(TAG, "decodeH264:===outIndex= "+ outIndex);
+
+//音视频   裁剪一段 true  1    false   2
+            if (outIndex >= 0) {
+//                try {
+//                    Thread.sleep(33);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+                long currentTimeMillis = System.currentTimeMillis();
+                long useTime           = currentTimeMillis - lastTimeMillis;
+                Log.d(TAG, "decodeH264: useTime=" + useTime);
+                lastTimeMillis = currentTimeMillis;
+
+                mediaCodec.releaseOutputBuffer(outIndex, true);
+            } else {
+//视频同步  不能  做到  1ms    60ms 差异   3600ms
+            }
+
+        }
+
+    }
+
+    private int findByFrame(byte[] bytes, int start, int totalSize) {
+
+        int j = 0;
+        for (int i = start; i < totalSize - 4; i++) {
+            if (bytes[i] == 0x00 && bytes[i + 1] == 0x00 && bytes[i + 2] == 0x00 && bytes[i + 3] == 0x01) {
+                return i;
+            }
+
+        }
+        return -1;
+    }
+
+    public byte[] getBytes(String path) throws IOException {
+        InputStream           is   = new DataInputStream(new FileInputStream(new File(path)));
+        int                   len;
+        int                   size = 1024;
+        byte[]                buf;
+        ByteArrayOutputStream bos  = new ByteArrayOutputStream();
+        buf = new byte[size];
+        while ((len = is.read(buf, 0, size)) != -1)
+            bos.write(buf, 0, len);
+        buf = bos.toByteArray();
+        return buf;
+    }
+
+
+    boolean useAsyncDecode = false;
+    int     startIndex     = 0;
+    int     totalSize      = 0;
+
+    MediaCodec.Callback callback = new MediaCodec.Callback() {
+        @Override
+        public void onInputBufferAvailable(@NonNull MediaCodec codec, int index) {
+            Log.d(TAG, "onInputBufferAvailable: index=" + index);
+
+            if (totalSize == 0 || startIndex >= totalSize) {
+                codec.queueInputBuffer(
+                        index,
+                        0,
+                        0,
+                        0,
+                        MediaCodec.BUFFER_FLAG_END_OF_STREAM
+                );
+            }
+
+            if (index >= 0) {
+
+                int nextFrameStart = findByFrame(bytes, startIndex + 2, totalSize);
+
+                ByteBuffer inputBuffer = codec.getInputBuffer(index);
+
+
+                inputBuffer.clear();
+                inputBuffer.put(bytes, startIndex, nextFrameStart - startIndex);
+
+                mediaCodec.queueInputBuffer(index, 0, nextFrameStart - startIndex, 0, 0);
+                startIndex = nextFrameStart;
+            }
+        }
+
+        @Override
+        public void onOutputBufferAvailable(@NonNull MediaCodec codec, int index, @NonNull MediaCodec.BufferInfo info) {
+            Log.d(TAG, "onOutputBufferAvailable: ");
+
+            long currentTimeMillis = System.currentTimeMillis();
+            long useTime           = currentTimeMillis - lastTimeMillis;
+            Log.d(TAG, "decodeH264: useTime=" + useTime);
+            lastTimeMillis = currentTimeMillis;
+
+            mediaCodec.releaseOutputBuffer(index, true);
+        }
+
+        @Override
+        public void onError(@NonNull MediaCodec codec, @NonNull MediaCodec.CodecException e) {
+            Log.d(TAG, "onError: ");
+        }
+
+        @Override
+        public void onOutputFormatChanged(@NonNull MediaCodec codec, @NonNull MediaFormat format) {
+            Log.d(TAG, "onOutputFormatChanged: ");
+        }
+    };
+}
---
+++ app/src/main/java/com/maniu/h264player/MainActivity.java
@@ -0,0 +1,73 @@
+package com.maniu.h264player;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    public boolean checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, 1);
+
+        }
+        return false;
+    }
+
+    H264Player2 h264Player;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        checkPermission();
+        initSurface();
+//        dsp  1   图形  视频 一个专用芯片  cpu  2
+//        MediaPlayer mediaPlayer = new MediaPlayer();
+//        try {
+//            mediaPlayer.setDataSource("input.mp4");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+    }
+
+    private void initSurface() {
+        SurfaceView surface = (SurfaceView) findViewById(R.id.preview);
+        final SurfaceHolder surfaceHolder = surface.getHolder();
+        surfaceHolder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder holder) {
+                h264Player = new H264Player2(MainActivity.this,
+//                        new File(Environment.getExternalStorageDirectory(), "out.h264").getAbsolutePath(),
+//                        new File(Environment.getExternalStorageDirectory(), "source.h264").getAbsolutePath(),
+//                        new File(Environment.getExternalStorageDirectory(), "test1280.h264").getAbsolutePath(),
+                        new File(Environment.getExternalStorageDirectory(), "tmp22.h264").getAbsolutePath(),
+                        surfaceHolder.getSurface());
+                h264Player.play();
+            }
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+            }
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
+            }
+        });
+
+    }
+
+}
