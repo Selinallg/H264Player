fb1697e65b5ac38e90552ca904ec2140cd07f0fa
dell-liu.lg@163.com <liu.lg@163.com>
2021-11-08 09:59:11 +0800

    确认解码器 ok

--- app/src/main/java/com/llg/h264player/MainActivity.java
+++ app/src/main/java/com/llg/h264player/MainActivity.java
@@ -5,15 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.media.MediaCodecInfo;
+import android.media.MediaCodecList;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
     public boolean checkPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(
                 Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
@@ -27,12 +33,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     H264Player2 h264Player;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         checkPermission();
         initSurface();
+        checkMediaCodec();
 //        dsp  1   图形  视频 一个专用芯片  cpu  2
 //        MediaPlayer mediaPlayer = new MediaPlayer();
 //        try {
@@ -43,21 +51,65 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void checkMediaCodec() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            MediaCodecList   list         = new MediaCodecList(MediaCodecList.REGULAR_CODECS);
+            MediaCodecInfo[] supportCodes = list.getCodecInfos();
+            Log.i(TAG, "解码器列表：");
+            for (MediaCodecInfo codec : supportCodes) {
+                if (!codec.isEncoder()) {
+                    String name = codec.getName();
+                    if (name.startsWith("OMX.google")) {
+                        Log.i(TAG, "软解->" + name);
+                    }
+                }
+            }
+            for (MediaCodecInfo codec : supportCodes) {
+                if (!codec.isEncoder()) {
+                    String name = codec.getName();
+                    if (!name.startsWith("OMX.google")) {
+                        Log.i(TAG, "硬解->" + name);
+                    }
+                }
+            }
+            Log.i(TAG, "编码器列表：");
+            for (MediaCodecInfo codec : supportCodes) {
+                if (codec.isEncoder()) {
+                    String name = codec.getName();
+                    if (name.startsWith("OMX.google")) {
+                        Log.i(TAG, "软编->" + name);
+                    }
+                }
+            }
+            for (MediaCodecInfo codec : supportCodes) {
+                if (codec.isEncoder()) {
+                    String name = codec.getName();
+                    if (!name.startsWith("OMX.google")) {
+                        Log.i(TAG, "硬编->" + name);
+                    }
+                }
+            }
+        }
+    }
+
     private void initSurface() {
-        SurfaceView surface = (SurfaceView) findViewById(R.id.preview);
+        SurfaceView         surface       = (SurfaceView) findViewById(R.id.preview);
         final SurfaceHolder surfaceHolder = surface.getHolder();
         surfaceHolder.addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(@NonNull SurfaceHolder holder) {
                 h264Player = new H264Player2(MainActivity.this,
-//                        new File(Environment.getExternalStorageDirectory(), "out.h264").getAbsolutePath(),
-                        new File(Environment.getExternalStorageDirectory(), "output.h265").getAbsolutePath(),
+                        new File(Environment.getExternalStorageDirectory(), "out.h264").getAbsolutePath(),
+//                        new File(Environment.getExternalStorageDirectory(), "output.h265").getAbsolutePath(),
                         surfaceHolder.getSurface());
                 h264Player.play();
             }
+
             @Override
             public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
             }
+
             @Override
             public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
 
